e5eac30be9ab8cc60193029f7b4acf8c4e4b058b
slayter11 <slayter@abv.bg>
2016-09-20 22:57:31 +0300

    User menu was cleaned

---
+++ app/local/KerstnerAt/FilterCustomerAccountNavigation/Block/Account/Navigation.php
@@ -0,0 +1,7 @@
+<?php
+class KerstnerAt_FilterCustomerAccountNavigation_Block_Account_Navigation extends Mage_Customer_Block_Account_Navigation {
+  public function removeLinkByName($name) {
+    unset($this->_links[$name]);
+  }
+}
+?>
